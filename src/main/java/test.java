f25f7038dbb41d6a533e93ba4b77947133f8a2c2
Michael W <welchm27@gmail.com>
2023-08-01 18:22:48 -0400

    test file created

---
+++ src/main/java/test.java
@@ -0,0 +1,18 @@
+class test {
+    // static methods (if any)
+
+    // instance variables
+
+    // constructors
+
+    // accessor methods
+
+    // business methods
+
+    // compareTo (ifAny)
+    // hashCode (ifAny)
+    // equals (ifAny)
+
+    // toString
+
+}   // END OF CLASS
