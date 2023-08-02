fca77169a37443bfc4269f698b431f00425cebf6
exxon suarez <suarez.exxon@gmail.com>
2023-08-02 14:41:57 -0700

    added new game functionality to EAZ class, AB#159

--- src/main/java/Controller/EAZ.java
+++ src/main/java/Controller/EAZ.java
@@ -4,15 +4,29 @@ import java.util.Scanner;
 
 public class EAZ {
 
-    boolean runGame = true;
+    public static boolean runGame;
 
-    public void quitGame() {
+
+    public static void quitGame() {
         System.out.println("Are you sure you want to quit?");
         String quitResponse = TextParser.GetInput();
         if(quitResponse == "yes") {
             runGame = false;
         } else {
             return;
+        }
+    }
+
+    public static void run() {
+        System.out.println("Would you like to play?");
+        String newGameOption = TextParser.GetInput();
+        if(newGameOption == "yes") {
+            while(runGame) {
+                // Have game logic here
+            }
+
+        } else {
+            quitGame();
         }
     }
 }
--- src/main/java/Main/main.java
+++ src/main/java/Main/main.java
@@ -1,11 +1,10 @@
 package Main;
-
+import Controller.EAZ;
 import Controller.StoryText;
 
 public class main {
     public static void main(String[] args) {
         StoryText.introScreen();
-
-
+        EAZ.run();
     }
 }   // END OF CLASS
