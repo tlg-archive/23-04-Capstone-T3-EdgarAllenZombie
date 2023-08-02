49f22a3fd46f2ecd0dceb788654711150a61a2db
Michael W <welchm27@gmail.com>
2023-08-02 16:42:57 -0400

    Clear screen, and present player with title screen upon load, AB#157

--- src/main/java/Controller/StoryText.java
+++ src/main/java/Controller/StoryText.java
@@ -1,10 +1,46 @@
 package Controller;
 
-class StoryText {
+public class StoryText {
 
-    public static void TitleScreen(){
-        System.out.printf("");
+    public static void introScreen(){
+        clearScreen();
+        titleScreen();
     }
 
+    public static void clearScreen(){
+        System.out.println("");System.out.println("");System.out.println("");System.out.println("");System.out.println("");
+        System.out.println("");System.out.println("");System.out.println("");System.out.println("");System.out.println("");
+        System.out.println("");System.out.println("");System.out.println("");System.out.println("");System.out.println("");
+        System.out.println("");System.out.println("");System.out.println("");System.out.println("");System.out.println("");
+        System.out.println("");System.out.println("");System.out.println("");System.out.println("");System.out.println("");
+        System.out.println("");System.out.println("");System.out.println("");System.out.println("");System.out.println("");
+        System.out.println("");System.out.println("");System.out.println("");System.out.println("");System.out.println("");
+        System.out.println("");System.out.println("");System.out.println("");System.out.println("");System.out.println("");
+    }
+
+    public static void titleScreen() {
+
+        System.out.printf("" +
+                " ___       __   _______   ___       ________  ________  _____ ______   _______           _________  ________         \n" +
+                "|\\  \\     |\\  \\|\\  ___ \\ |\\  \\     |\\   ____\\|\\   __  \\|\\   _ \\  _   \\|\\  ___ \\         |\\___   ___\\\\   __  \\        \n" +
+                "\\ \\  \\    \\ \\  \\ \\   __/|\\ \\  \\    \\ \\  \\___|\\ \\  \\|\\  \\ \\  \\\\\\__\\ \\  \\ \\   __/|        \\|___ \\  \\_\\ \\  \\|\\  \\       \n" +
+                " \\ \\  \\  __\\ \\  \\ \\  \\_|/_\\ \\  \\    \\ \\  \\    \\ \\  \\\\\\  \\ \\  \\\\|__| \\  \\ \\  \\_|/__           \\ \\  \\ \\ \\  \\\\\\  \\      \n" +
+                "  \\ \\  \\|\\__\\_\\  \\ \\  \\_|\\ \\ \\  \\____\\ \\  \\____\\ \\  \\\\\\  \\ \\  \\    \\ \\  \\ \\  \\_|\\ \\           \\ \\  \\ \\ \\  \\\\\\  \\     \n" +
+                "   \\ \\____________\\ \\_______\\ \\_______\\ \\_______\\ \\_______\\ \\__\\    \\ \\__\\ \\_______\\           \\ \\__\\ \\ \\_______\\    \n" +
+                "    \\|____________|\\|_______|\\|_______|\\|_______|\\|_______|\\|__|     \\|__|\\|_______|            \\|__|  \\|_______|    \n" +
+                "                                                                                                                     \n" +
+                "                                                                                                                     \n" +
+                "                                                                                                                     \n" +
+                "                                            _______   ________  ________                                             \n" +
+                "                                           |\\  ___ \\ |\\   __  \\|\\_____  \\                                            \n" +
+                "                                           \\ \\   __/|\\ \\  \\|\\  \\\\|___/  /|                                           \n" +
+                "                                            \\ \\  \\_|/_\\ \\   __  \\   /  / /                                           \n" +
+                "                                             \\ \\  \\_|\\ \\ \\  \\ \\  \\ /  /_/__                                          \n" +
+                "                                              \\ \\_______\\ \\__\\ \\__\\\\________\\                                        \n" +
+                "                                               \\|_______|\\|__|\\|__|\\|_______|                                        \n" +
+                "                                                                                                                     \n" +
+                "                                                                                \n" +
+                "                                    Uncover your dark side and grow to epic proportions!     \n");
+    }
 
 }   // END OF CLASS
