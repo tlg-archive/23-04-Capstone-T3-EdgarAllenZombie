9193902b901ee0d1f79be92be5310c4a656334ee
mendezanderson <kevin.mendezanderson@tlgcohort.com>
2023-08-07 14:57:52 -0400

    deleting some comments

--- src/main/java/Controller/Location.java
+++ src/main/java/Controller/Location.java
@@ -19,22 +19,4 @@ public class Location {
 //        this.directions = new HashMap<>();
     }
 
-//    public void addDirection(String direction, String destination) {
-//        directions.put(direction.toLowerCase(), destination);
-//    }
-
-    // Getters and Setters (You can use Lombok or manually create them if needed)
-
-//    public static void main(String[] args) {
-//        // Sample usage to create a location
-//        Location location = new Location("Fayer", "This is the starting room.");
-//        location.addDirection("north", "north_room");
-//        location.addDirection("east", "east_room");
-//        location.addDirection("south", "south_room");
-//        location.addDirection("west", "west_room");
-//
-//        Gson gson = new Gson();
-//        String locationJson = gson.toJson(location);
-//        System.out.println(locationJson);
-//    }
 }
