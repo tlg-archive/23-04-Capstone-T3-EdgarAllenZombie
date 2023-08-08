d733e4823f5658f342a58c5186083bc8069ce389
mendezanderson <kevin.mendezanderson@tlgcohort.com>
2023-08-08 16:50:18 -0400

    Re factor code in order to have better architecture

--- src/main/java/eaz/model/JsonReader.java
+++ src/main/java/eaz/model/JsonReader.java
@@ -1,7 +1,6 @@
 package eaz.model;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,57 +8,14 @@ import java.io.Reader;
 
 public class JsonReader {
 
-    public static String currentLocationName = "Foyer";
-
-    public static LocationData getLocation() throws IOException {
-        return readJson(LocationData.class);
-    }
-
-    public static <T> T readJson(Class<T> clazz) throws IOException {
+    public static <T> T readJson(Class<T> clazz, String filename) throws IOException {
         Gson gson = new Gson();
-        String filename = "JsonObjects.json";
         try (Reader reader = new InputStreamReader(JsonReader.class.getClassLoader().getResourceAsStream(filename))) {
             return gson.fromJson(reader, clazz);
         }
     }
 
-    public static void move(String noun) throws IOException {
-        LocationData locationData = getLocation();
-        for (Locations location : locationData.locations) {
-            if (location.name.equals(currentLocationName)) {
-                if (location.directions.has(noun)) {
-                    currentLocationName = location.directions.get(noun).getAsString();
-                    System.out.println("You have moved to " + currentLocationName + ".");
-                    return;
-                }
-            }
-        }
-        System.out.println("You can't go that way.");
-    }
-
-    public static Locations getLocationByName(String currentLocationName) throws IOException {
-//        return readJson(LocationData.class);
-        LocationData locationData = readJson(LocationData.class);
-        for(Locations location : locationData.locations) {
-            if(location.name.equalsIgnoreCase(currentLocationName)){
-                return location;
-            }
-        }
-        return null;
-    }
-
-    public static String getCurrentLocationName() {
-        return currentLocationName;
-    }
-
-    static public class LocationData {
-        Locations[] locations;
-    }
-
-    static public class Locations {
-        String name;
-        JsonObject directions;
-        public String description;
-        String[] items;
+    public static Mansion readMansion() throws IOException {
+        return readJson(Mansion.class, "JsonObjects.json");
     }
 }
--- src/main/java/eaz/model/Look.java
+++ src/main/java/eaz/model/Look.java
@@ -4,20 +4,19 @@ import eaz.view.StoryText;
 
 import java.io.IOException;
 
-import static eaz.model.JsonReader.*;
-import static java.util.Objects.requireNonNull;
 
 public class Look {
     private static String item;
 
-    public static void look(String noun) throws IOException {
-        LocationData locationData = new LocationData();
+    public static void look(String noun, Location location) throws IOException {
         StoryText.printDoubleLine();
-        if (noun.equals("room")){
-            System.out.println(requireNonNull(getLocationByName(currentLocationName)).description);
-        }else
-        System.out.println("look at " + noun);
+        if (noun.equals("room")) {
+            System.out.println(location.getDescription());
+        } else {
+            System.out.println("look at " + noun);
+        }
         // use that user input to call JsonReader getItem function and retrieve nested description
+
     }
 
 }
