2d242b7268c6ff9e34670497a73c6be92d0b5951
mendezanderson <kevin.mendezanderson@tlgcohort.com>
2023-08-11 10:58:30 -0400

    Created the volumeUp and Volume Down functions, AB#196

--- src/main/java/eaz/controller/TextParser.java
+++ src/main/java/eaz/controller/TextParser.java
@@ -81,6 +81,12 @@ public class TextParser {
             case "hit":
                 mansion.fight(noun);
                 break;
+            case "off":
+                mansion.getBackgroundMusic().stop();
+                break;
+            case "on":
+                mansion.getBackgroundMusic().play();
+                break;
             default:
                 System.out.println("Invalid command. Try again.");
                 viewMain.textHelp();
--- src/main/java/eaz/view/Music.java
+++ src/main/java/eaz/view/Music.java
@@ -3,17 +3,22 @@ package eaz.view;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import java.io.File;
 
 public class Music {
     private Clip clip;
+    private FloatControl volumeControl;
 
     public Music(String filePath) {
         try {
-            File audioFile = new File("src/main/resources/MapleStory_RainDrops.mp3");
+            File audioFile = new File(filePath);
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
             clip = AudioSystem.getClip();
             clip.open(audioStream);
+
+            // Initialize volume control
+            volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -28,7 +33,26 @@ public class Music {
     public void stop() {
         if (clip != null && clip.isRunning()) {
             clip.stop();
+            clip.setFramePosition(0); 
             clip.close();
+        }
+    }
+
+    public void volumeUp() {
+        if (volumeControl != null) {
+            float currentVolume = volumeControl.getValue();
+            float maxVolume = volumeControl.getMaximum();
+            float newVolume = Math.min(currentVolume + 1.0f, maxVolume);
+            volumeControl.setValue(newVolume);
+        }
+    }
+
+    public void volumeDown() {
+        if (volumeControl != null) {
+            float currentVolume = volumeControl.getValue();
+            float minVolume = volumeControl.getMinimum();
+            float newVolume = Math.max(currentVolume - 1.0f, minVolume);
+            volumeControl.setValue(newVolume);
         }
     }
 }
