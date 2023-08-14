1833e673d90052b7097adea07d48604fdc1424b5
welch <welchm27@gmail.com>
2023-08-14 13:37:24 -0400

    Random engine for attacks succeeding and combat loop while player and enemy above 0 health, AB#205

--- src/main/java/eaz/model/Combat.java
+++ src/main/java/eaz/model/Combat.java
@@ -14,35 +14,47 @@ public class Combat {
         Character c = getTarget(target, currentLocation, characters);
         assert c != null;
 
-        System.out.println("Would you like to attack? (yes/no)");
-        String attackChoice = TextParser.getInput();
+        System.out.println("Beginning combat");
 
-        if (attackChoice.equals("yes")) {
-            checkForHit(c, player);
-            System.out.println();
-            viewMain.displayEnemyHealth(c);
-            enemyAttack(c, player);
-            viewMain.displayPlayerHealth(player);
+        // run combat loop
+        while (player.getHealth() > 0 && c.getHealth() > 0) {
+            System.out.println("Would you like to attack? (yes/no)");
+            String attackChoice = TextParser.getInput();
+            viewMain.clearScreen();
+            if (attackChoice.equals("yes")) {
+                checkForHit(c, player);
+                System.out.println();
+                viewMain.displayEnemyHealth(c);
+                enemyAttack(c, player);
+                viewMain.displayPlayerHealth(player);
+            }else if(attackChoice.equals("no")){
+                return;
+            }
+            if(c.getHealth() <= 0){
+                String[] enemies = {};
+                currentLocation.setCharacters(enemies);
+            }
         }
-//        viewMain.displayCombat(c, player, hit);
-//        viewMain.displayCombatHealth(c);
-
 
     }
 
-    private Character getTarget(String target, Location currentLocation, Character[] characters){
-        if(currentLocation.getCharacters() != null){
-            for(String characterName : currentLocation.getCharacters()){
-                for(Character c : characters){
-                    if(characterName.equals(c.getName()) && characterName.equals(target)){
+
+//    }
+
+    private Character getTarget(String target, Location currentLocation, Character[] characters) {
+        if (currentLocation.getCharacters() != null) {
+            for (String characterName : currentLocation.getCharacters()) {
+                for (Character c : characters) {
+                    if (characterName.equals(c.getName()) && characterName.equals(target)) {
                         return c;
                     }
                 }
             }
-        } return null;
+        }
+        return null;
     }
 
-    private void checkForHit(Character c, Player player){
+    private void checkForHit(Character c, Player player) {
         boolean hit = false;
         int pHit = player.getHitChance();  // save player hit
         int hitRoll = Randomizer.randomizer(20);  // save random hit chance
@@ -50,16 +62,16 @@ public class Combat {
         int targetArmor = c.getArmor();  // get character armor value
         int damage = player.getDamage();
 
-        if(totalHit > targetArmor){  // compare it to enemy armor value
+        if (totalHit > targetArmor) {  // compare it to enemy armor value
             // display output to player
             viewMain.combatHit(c, pHit, hitRoll, totalHit, true, damage);
             c.setHealth(damage);
-        }else{
+        } else {
             viewMain.combatHit(c, pHit, hitRoll, totalHit, false, damage);
         }
     }
 
-    private void enemyAttack(Character c, Player player){
+    private void enemyAttack(Character c, Player player) {
         boolean hit = false;
         int eHit = c.getHit();  // save enemy hit chance
         int hitRoll = Randomizer.randomizer(20);
@@ -67,10 +79,10 @@ public class Combat {
         int playerArmor = player.getArmor();
         int damage = c.getDamage();
 
-        if(totalHit > playerArmor){
+        if (totalHit > playerArmor) {
             viewMain.enemyHit(c, eHit, hitRoll, totalHit, true, damage);
             player.setHealth(damage);
-        }else{
+        } else {
             viewMain.enemyHit(c, eHit, hitRoll, totalHit, false, damage);
         }
     }
