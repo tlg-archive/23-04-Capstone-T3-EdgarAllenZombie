ddd38874450d5b85e64fab9138f3b86f8d0997a2
Richard Ropac <Richard.Ropac@tlgcohort.com>
2023-08-18 23:46:27 -0500

    The map is displayed in a persistent way, AB#616

--- src/main/java/eaz/controller/GUIFunctionality.java
+++ src/main/java/eaz/controller/GUIFunctionality.java
@@ -6,6 +6,7 @@ import org.w3c.dom.Text;
 
 import javax.swing.*;
 import javax.swing.text.JTextComponent;
+import java.awt.*;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
@@ -13,10 +14,6 @@ import java.io.PrintStream;
 class GUIFunctionality {
     //methods to incorporate the original game into the GUI go here.
     private final Mansion mansion;
-//    ByteArrayOutputStream basicOutput = new ByteArrayOutputStream();
-//    PrintStream printOutput = new PrintStream(basicOutput);
-//    System.setOut(printOutput);
-
 
     GUIFunctionality(Mansion mansion) {
         this.mansion = mansion;
@@ -28,45 +25,33 @@ class GUIFunctionality {
         PrintStream printOutput = new PrintStream(basicOutput);
         System.setOut(printOutput);
 
-
-
         //This is from the user input in the panel and sending it into the text parser
         String input = field.getText();
+        field.setText("");
+
         try {
             TextParser.handleInput(mansion, input, "");
         } catch (IOException e) {
             e.printStackTrace();
         }
         String capturedOutput = basicOutput.toString();
-
-//        capturedOutput = capturedOutput.replaceAll("\\d{1,2}(;\\d{1,2})?", "");
         capturedOutput = capturedOutput.replaceAll("\\x1B\\[[0-9;]*[mK]", "")
                 .replaceAll("\\[\\s*\\]", "");
 
 
+        Font font = new Font("Monospaced", Font.PLAIN, 12);
+        JTextArea textArea = new JTextArea(capturedOutput);
+        textArea.setFont(font);
+        textArea.setTabSize(1);
+
         // TODO: 8/18/2023 involk methods in EAZ and/or text parser to handle user input
-
-        //clears the panel input
-        field.setText("");
-
-        //this just appends the line to the basicOutput and added it as testing
-        System.out.println("Howdy Ho and Away we Go");
-        //System.out.println(input);
-        //if (input.equalsIgnoreCase("map")) {
-            //JOptionPane.showMessageDialog(null, "Commands: | move | take | attack | talk | help | quit");
 
         //The basicOutput holds whatever info was sout in the game and the toString will direct it to our panels/windows
         //JOptionPane.showMessageDialog(null, basicOutput.toString());
-        JOptionPane.showMessageDialog(null, capturedOutput);
-        //}
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        JPanel panel = new JPanel();
+        panel.add(scrollPane);
+        //JOptionPane.showMessageDialog(null, capturedOutput);
+        JOptionPane.showMessageDialog(null, panel, input.toUpperCase(), JOptionPane.PLAIN_MESSAGE);
     }
-//
-//    void redirectingOutput() {
-//        ByteArrayOutputStream basicOutput = new ByteArrayOutputStream();
-//        PrintStream printOutput = new PrintStream(basicOutput);
-//        basicOutput.toString(); //allows the gui to print
-//    }
-////
-//    System.setOut(printOutput);
-//    basicOutput.reset();
 }
