be21c2ca58a0d947e9e820ebb20a6ff1066ffaf2
Richard Ropac <Richard.Ropac@tlgcohort.com>
2023-08-25 19:21:14 -0500

    Player see an image of the room they are in within game window, AB#745

---
+++ src/main/java/eaz/view/Entity.java
@@ -0,0 +1,14 @@
+package eaz.view;
+
+import java.awt.image.BufferedImage;
+
+public class Entity {
+
+    public int x, y;
+    public int speed;
+
+    public BufferedImage charZombie;
+    public String direction;
+
+
+}
